eda6ce85309706e627fee13a263e71298e79d462
lucasgabrieloli <lucas.sousa319@etec.sp.gov.br>
2025-05-08 14:30:19 -0300

    commit

---
+++ app/perfil.tsx
@@ -0,0 +1,22 @@
+import {View, Text, StyleSheet} from 'react-native'
+
+export default function perfil(){
+return(
+
+    <View>
+<Text>Post</Text>
+
+</View>
+)
+
+}
+
+const styles = StyleSheet.create({
+    root:{
+        flex:1,
+    },
+    container:{
+        flex: 1,
+        backgroundColor: "#3db342",
+    },
+})
