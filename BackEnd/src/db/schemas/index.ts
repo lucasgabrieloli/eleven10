e686bc834c6e88af19afeb48f78cd142747dacfd
LucasEmanuel08 <lucas.silva4383@etec.sp.gov.br>
2025-06-01 00:33:03 -0300

    Banco de dados

--- BackEnd/src/db/schemas/index.ts
+++ BackEnd/src/db/schemas/index.ts
@@ -1,87 +1,109 @@
-// src/db/schema.ts
+
 import { pgTable, integer, varchar, timestamp, boolean,  text, unique } from 'drizzle-orm/pg-core';
 import { pgEnum } from 'drizzle-orm/pg-core';
 import { createId } from '@paralleldrive/cuid2'
+import { copyFile, cp } from 'fs';
+import { customType } from 'drizzle-orm/pg-core';
+import { int } from 'drizzle-orm/mysql-core';
 
-export const tipoUsuarioEnum = pgEnum('tipo_usuario', ['Atleta', 'Olheiro']);
-export const generoEnum = pgEnum('genero', ['Masculino', 'Feminino', 'Não binário']);
-export const modalidadeEnum = pgEnum('modalidade', ['Masculino', 'Feminino']);
-export const criterioEnum = pgEnum('criterio', ['Mais curtidos', 'Mais visualizados']);
-
-export const usuario = pgTable('usuario', {
-  idUsuario: text('id_usuario').primaryKey().$defaultFn(() => createId()),
-  username: varchar('username', { length: 45 }).notNull(),
-  senha: varchar('senha', { length: 45 }).notNull(),
-  email: varchar('email', { length: 100 }).notNull().unique(),
-  telefone: varchar('telefone', { length: 15 }),
-  tipoUsuario: tipoUsuarioEnum('tipo_usuario').notNull(),
-  nomeCompleto: varchar('nome_completo', { length: 255 }).notNull(),
-  genero: generoEnum('genero').notNull(),
-  dataDeAniversario: timestamp('data_de_aniversario').notNull(),
-  profilePicture: varchar('profile_picture', { length: 100 }).notNull(),
-  logradouro: varchar('logradouro', { length: 100 }).notNull(),
-  verificado: boolean('verificado').notNull().default(false),
-  criadoEm: timestamp('criado_em').notNull().defaultNow(),
-  atualizadoEm: timestamp('atualizado_em').notNull().defaultNow(),    
+const bytea = customType<{ data: Buffer; notNull: true }>({
+  dataType() {
+    return 'bytea';
+  },
 });
+
+
+export const generoEnum = pgEnum('genero', ['Masculino', 'Feminino']);
+
+
 
 export const atleta = pgTable('atleta', {
   idAtleta: text('id_atleta').primaryKey().$defaultFn(() => createId()),
-  modalidade: modalidadeEnum('modalidade'),
-  biografia: text('biografia').notNull(),
-  conquistas: text('conquistas').notNull(),
-  posicao: varchar('posicao', { length: 50 }).notNull(),
-  federacao: text('federacao').notNull(),
-  usuarioIdUsuario:text('atleta_id_usuario').references(() => usuario.idUsuario),
+  username: varchar('username', { length: 30 }).notNull().unique(),
+  senha: varchar('senha', { length: 24 }).notNull(),
+  email: varchar('email', { length: 100 }).notNull().unique(),
+  nomeCompleto: varchar('nome_completo', { length: 50 }).notNull(),
+  dataNascimento: timestamp('data_nascimento').notNull(),
+  genero: generoEnum('genero').notNull(),
+  telefone: varchar('telefone', { length: 15 }).notNull(),
+  logadouro: varchar('logadouro', { length: 100 }).notNull(),
+  criadoEm: timestamp('criado_em').notNull().defaultNow(),
 });
 
 export const olheiro = pgTable('olheiro', {
   idOlheiro: text('id_olheiro').primaryKey().$defaultFn(() => createId()),
-  usuarioIdUsuario:text('usuario_id_usuario').references(() => usuario.idUsuario),
-  areaDeInteresses: text('area_de_interesses').notNull(),
-  verificar: integer('verificar').notNull().default(0),
-  informacoesContato: text('informacoes_contato').notNull(),
+  username: varchar('username', { length: 30 }).notNull().unique(),
+  senha: varchar('senha', { length: 24 }).notNull(),
+  email: varchar('email', { length: 100 }).notNull().unique(),
+  nomeCompleto: varchar('nome_completo', { length: 50 }).notNull(),
+  cpf: varchar('cpf', { length: 11 }).notNull().unique(),
+  dataNascimento: timestamp('data_nascimento').notNull(),
+  genero: generoEnum('genero').notNull(),
+  telefone: varchar('telefone', { length: 15 }).notNull(),
+  logadouro: varchar('logadouro', { length: 100 }).notNull(),
+  verificado: boolean('verificado').notNull().default(false),
 });
 
 export const eventos = pgTable('eventos', {
   idEventos: text('id_eventos').primaryKey().$defaultFn(() => createId()),
-  usuarioIdUsuario: text('usuario_id_usuario').references(() => usuario.idUsuario),
-  nome: varchar('nome', { length: 255 }).notNull(),
-  localizacao: varchar('localizacao', { length: 255 }).notNull(),
-  data1: timestamp('data_1').notNull(),
-  data2: timestamp('data_2').notNull(),
-  criadoEm: timestamp('criado_em').notNull().defaultNow(),
+  idOlheiro: text('id_olheiro').notNull().references(() => olheiro.idOlheiro),
+  nomeTime: varchar('nome_time', { length: 30 }).notNull(),
+  dataEvento: timestamp('data_evento').notNull(),
+  local: varchar('local', { length: 100 }).notNull(),
+  horario: timestamp('horario').notNull(),
+  categoria: varchar('categoria', { length: 20 }).notNull(),
+  posicoes: varchar('posicoes', { length: 100 }).notNull(),
 });
 
-export const publicacoes = pgTable('publicacoes', {
-  idPublicacoes: text('id_publicacoes').primaryKey().$defaultFn(() => createId()),
-  usuarioIdUsuario: text('usuario_id_usuario').references(() => usuario.idUsuario),
-  conteudo: text('conteudo').notNull(),
-  midiaUrl: varchar('midia_url', { length: 255 }).notNull(),
+export const posts = pgTable('posts', {
+  idPosts: text('id_posts').primaryKey().$defaultFn(() => createId()),
+  idAtleta: text('id_atleta').notNull().references(() => atleta.idAtleta),
+  idOlheiro: text('id_olheiro').notNull().references(() => olheiro.idOlheiro),
+  midia: bytea ('midia') .notNull(),
+  legenda: text('legenda').notNull(),
   criadoEm: timestamp('criado_em').notNull().defaultNow(),
 });
 
 export const curtida = pgTable('curtida', {
   idCurtida: text('id_curtida').primaryKey().$defaultFn(() => createId()),
-  usuarioIdUsuario: text('usuario_id_usuario').notNull().references(() => usuario.idUsuario),
-  publicacoesIdPublicacoes: text('publicacoes_id_publicacoes').notNull().references(() => publicacoes.idPublicacoes),
-  data: timestamp('data').notNull().defaultNow(),
+  idposts: text('id_posts').notNull().references(() => posts.idPosts),
+  idAtleta: text('id_atleta').notNull().references(() => atleta.idAtleta),
+  idOlheiro: text('id_olheiro').notNull().references(() => olheiro.idOlheiro),
+  criadoem: timestamp('criado_em').notNull().defaultNow(),
 });
 
-export const favoritos = pgTable('favoritos', {
-  idFavoritos: text('id_favoritos').primaryKey().$defaultFn(() => createId()),
+export const comentarios = pgTable('comentarios', {
+  idComentarios: text('id_comentarios').primaryKey().$defaultFn(() => createId()),
+  idPosts: text('id_posts').notNull().references(() => posts.idPosts),
+  idAtleta: text('id_atleta').notNull().references(() => atleta.idAtleta),
+  idOlheiro: text('id_olheiro').notNull().references(() => olheiro.idOlheiro),
+  conteudo: text('comentario').notNull(),
   criadoEm: timestamp('criado_em').notNull().defaultNow(),
 });
 
+export const perfil = pgTable('perfil', {
+  idPerfil: text('id_perfil').primaryKey().$defaultFn(() => createId()),
+  idAtleta: text('id_atleta').notNull().references(() => atleta.idAtleta),
+  fotoPerfil: bytea('foto_perfil').notNull(),
+  idPosts: text('id_posts').notNull().references(() => posts.idPosts),
+  bio: text('bio').notNull(),
+  favoritos: integer('favoritos').notNull().default(0),
+  favoritados: integer('favoritados').notNull().default(0),
+  contadorPosts: integer('contador_posts').notNull().default(0),
+});
 
-export const usuarioHasFavoritos = pgTable('usuario_has_favoritos', {
- usuarioIdUsuario: text('usuario_id_usuario').notNull().references(() => usuario.idUsuario),
-  favoritosIdFavoritos: text('favoritos_id_favoritos').notNull().references(() => favoritos.idFavoritos),
-}, );
-
-export const destaques = pgTable('destaques', {
-  idDestaques: text('id_destaques').primaryKey().$defaultFn(() => createId()),
-  publicacoesIdPublicacoes: text('publicacoes_id_publicacoes').notNull().references(() => publicacoes.idPublicacoes),
-  criterio: criterioEnum('criterio').notNull(),
-  criadoEm: timestamp('criado_em').notNull().defaultNow(),
+export const Curriculo = pgTable('curriculo', {
+  idCurriculo: text('id_curriculo').primaryKey().$defaultFn(() => createId()),
+  idAtleta: text('id_atleta').notNull().references(() => atleta.idAtleta),
+  nomeCompleto: varchar('nome_completo', { length: 50 }).notNull(),
+  idade: integer('idade').notNull(),
+  altura: varchar('altura', { length: 10 }).notNull(),
+  peso: varchar('peso', { length: 10 }).notNull(),
+  categoria: varchar('categoria', { length: 20 }).notNull(),
+  posicao: varchar('posicao', { length: 40 }).notNull(),
+  jogos: integer('jogos').notNull().default(0),
+  gols: integer('gols').notNull().default(0),
+  assistencias: integer('assistencias').notNull().default(0),
+  conquistas: text('conquistas').notNull(),
+  camisaFavorita: integer('camisa_favorita').notNull(),
 });
